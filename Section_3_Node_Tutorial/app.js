2be8030c33ea2df99d6339e2a73d17df17f469ea
Andre Ostan <andre.ostan@gmail.com>
2022-11-07 20:37:41 -0300

    Section 3.25 HTTP Module (more features)

--- Section_3_Node_Tutorial/app.js
+++ Section_3_Node_Tutorial/app.js
@@ -1,8 +1,14 @@
-const names = require('./4-names');
-const sayHi = require('./5-utils');
-const data = require('./6-alternative-flavor');
-require('./7-mind-grenade');
+const http = require('http');
 
-// sayHi('susan');
-// sayHi(names.andre);
-// sayHi(names.kaori);
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.end('Welcome to our home page');
+  } else if (req.url === '/about') {
+    res.end('Here is our short history');
+  } else
+    res.end(`<h1>Oops!</h1>
+  <p>We can't seem to find the page you are looking for</p>
+  <a href="/">back home</a>`);
+});
+
+server.listen(5000);
