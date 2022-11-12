c9a365bfdb15b0e491cd73efa082344a09f5307b
Andre Ostan <andre.ostan@gmail.com>
2022-11-12 14:04:15 -0300

    Section 4.75 Additional Middleware Info

--- Section_4_Express/final/09-middleware-use.js
+++ Section_4_Express/final/09-middleware-use.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan');
 const logger = require('./logger');
 const authorize = require('./authorize');
 // req => middleware => res
 
-// 1. use vs route
-// 2. options - our own / express / third party
-
-// app.use([logger, authorize]);
-// app.use(express.static('./public'));
-app.use(morgan('tiny'));
+app.use([logger, authorize]); // the order matters
+// api/home/about/products
 
 app.get('/', (req, res) => {
   res.send('Home');
