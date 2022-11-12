e57869901474e21341f1e6ff5ae8d26de267f402
Andre Ostan <andre.ostan@gmail.com>
2022-11-12 12:15:29 -0300

    Section 4.73 app.use()

--- Section_4_Express/app.js
+++ Section_4_Express/app.js
@@ -1,18 +1,25 @@
 const express = require('express');
 const app = express();
-const { products } = require('./data');
+const logger = require('./logger');
+// req => middleware => res
+
+app.use('/api', logger);
+// api/home/about/products
 
 app.get('/', (req, res) => {
-  res.send('<h1>Home Page</h1><a href="/api/products">Products</a>');
+  res.send('Home');
+});
+
+app.get('/about', (req, res) => {
+  res.send('About');
 });
 
 app.get('/api/products', (req, res) => {
-  const newProducts = products.map(product => {
-    const { id, name, image } = product;
-    return { id, name, image };
-  });
+  res.send('Products');
+});
 
-  res.json(newProducts);
+app.get('/api/items', (req, res) => {
+  res.send('Items');
 });
 
 app.listen(5000, () => {
