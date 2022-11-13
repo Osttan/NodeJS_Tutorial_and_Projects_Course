6de82c46dbcb9d7ee9164866b5cfcbde9234bd2d
Andre Ostan <andre.ostan@gmail.com>
2022-11-13 09:10:38 -0300

    End of Section 4

--- Section_4_Express/controllers/people.js
+++ Section_4_Express/controllers/people.js
@@ -1,21 +1,10 @@
-const express = require('express');
-const { restart } = require('nodemon');
-const app = express();
-let { people } = require('./data');
+let { people } = require('../data');
 
-// static assets
-app.use(express.static('./Section_4_Express/methods-public'));
-
-// parse form data
-app.use(express.urlencoded({ extended: false }));
-//parse json
-app.use(express.json());
-
-app.get('/api/people', (req, res) => {
+const getPeople = (req, res) => {
   res.status(200).json({ success: true, data: people });
-});
+};
 
-app.post('/api/people', (req, res) => {
+const createPerson = (req, res) => {
   const { name } = req.body;
   if (!name) {
     return res
@@ -23,9 +12,9 @@ app.post('/api/people', (req, res) => {
       .json({ success: false, msg: 'Please provide name value' });
   }
   res.status(201).json({ success: true, person: name });
-});
+};
 
-app.post('/api/postman/people', (req, res) => {
+const createPersonPostman = (req, res) => {
   const { name } = req.body;
   if (!name) {
     return res
@@ -33,17 +22,9 @@ app.post('/api/postman/people', (req, res) => {
       .json({ success: false, msg: 'Please provide name value' });
   }
   res.status(201).send({ success: true, data: [...people, name] });
-});
+};
 
-app.post('/login', (req, res) => {
-  const { name } = req.body;
-  if (name) {
-    return res.status(200).send(`Welcome ${name}`);
-  }
-  res.status(401).send('Please Provide Credentials ');
-});
-
-app.put('/api/people/:id', (req, res) => {
+const updatePerson = (req, res) => {
   const { id } = req.params;
   const { name } = req.body;
 
@@ -62,9 +43,9 @@ app.put('/api/people/:id', (req, res) => {
     return person;
   });
   res.status(200).json({ success: true, data: newPeople });
-});
+};
 
-app.delete('/api/people/:id', (req, res) => {
+const deletePerson = (req, res) => {
   const person = people.find(person => person.id === Number(req.params.id));
 
   if (!person) {
@@ -77,8 +58,12 @@ app.delete('/api/people/:id', (req, res) => {
     person => person.id !== Number(req.params.id)
   );
   return res.status(200).json({ success: true, data: newPeople });
-});
+};
 
-app.listen(5000, () => {
-  console.log('Server is listening on port 5000...');
-});
+module.exports = {
+  getPeople,
+  createPerson,
+  createPersonPostman,
+  updatePerson,
+  deletePerson,
+};
