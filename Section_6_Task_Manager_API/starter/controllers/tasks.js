8daee0cac259842c13775bd6e6d86193e2b2b99a
Andre Ostan <andre.ostan@gmail.com>
2022-11-14 08:44:53 -0300

    Section 4-111 Try/Catch Block

--- Section_6_Task_Manager_API/starter/controllers/tasks.js
+++ Section_6_Task_Manager_API/starter/controllers/tasks.js
@@ -1,6 +1,14 @@
+const Task = require('../models/task');
 const getAllTasks = (req, res) => res.send('get all tasks');
 
-const createTask = (req, res) => res.json(req.body);
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json(error.errors.name.message);
+  }
+};
 const getTask = (req, res) => res.json({ id: req.params.id });
 const updateTask = (req, res) => res.send('update task');
 const deleteTask = (req, res) => res.send('delete task');
