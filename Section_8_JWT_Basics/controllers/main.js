ab63e40a9e3c4e15b3eafde6c54359a38bb3fbc4
Andre Ostan <andre.ostan@gmail.com>
2022-11-16 19:18:20 -0300

    End of Section 8

--- Section_8_JWT_Basics/controllers/main.js
+++ Section_8_JWT_Basics/controllers/main.js
@@ -1,21 +1,31 @@
-const CustomAPIError = require('../errors/custom-error');
+const jwt = require('jsonwebtoken');
+const { BadRequestError } = require('../errors');
 
 const login = async (req, res) => {
   const { username, password } = req.body;
-  // mongo
-  // Joi
+  // mongoose validations
+  // Joi (package)
   // Check in the controller
 
   if (!username || !password) {
-    throw new CustomAPIError('Please provide e-mail and password', 400);
+    throw new BadRequestError('Please provide e-mail and password');
   }
-  res.send('Fake Login/Register/SignUp');
+  // just for demo, normally provided by DB!!!
+  const id = new Date().getDate();
+
+  // try to keep payload small, better experience for user
+  // just for demo, in production always use long, complex and unguessable string value!!!!!
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+  res.status(200).json({ msg: 'user created', token });
 };
 
 const dashboard = async (req, res) => {
   const luckyNumber = Math.floor(Math.random() * 100);
+
   res.status(200).json({
-    msg: `Hello John Doe`,
+    msg: `Hello ${req.user.username}`,
     secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
   });
 };
