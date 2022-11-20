c87e2b3bc7902af64fe29596c73c4da880802cd1
Andre Ostan <andre.ostan@gmail.com>
2022-11-20 17:40:36 -0300

    Section 10-239 Refactor Status Data

--- Section_10_Jobster_API/middleware/authentication.js
+++ Section_10_Jobster_API/middleware/authentication.js
@@ -14,7 +14,8 @@ const auth = async (req, res, next) => {
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET_JOBS_API);
     // attach the user to the job routes
-    req.user = { userId: payload.userId, name: payload.name };
+    const testUser = payload.userId === '6378fa28faa82c6d10aa6b60';
+    req.user = { userId: payload.userId, testUser };
     next();
   } catch (error) {
     throw new UnauthenticatedError('Authentication Invalid');
--- Section_10_Jobster_API/routes/jobs.js
+++ Section_10_Jobster_API/routes/jobs.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const testUser = require('../middleware/testUser');
 
 const {
   createJob,
@@ -7,9 +8,15 @@ const {
   getAllJobs,
   getJob,
   updateJob,
+  showStats,
 } = require('../controllers/jobs');
 
-router.route('/').post(createJob).get(getAllJobs);
-router.route('/:id').get(getJob).delete(deleteJob).patch(updateJob);
+router.route('/').post(testUser, createJob).get(getAllJobs);
+router.route('/stats').get(showStats);
+router
+  .route('/:id')
+  .get(getJob)
+  .delete(testUser, deleteJob)
+  .patch(testUser, updateJob);
 
 module.exports = router;
