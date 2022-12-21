9e69d0213a9f1f09eb7bae3739b7f29c5934171b
Andre Ostan <andre.ostan@gmail.com>
2022-12-21 19:19:52 -0300

    Section 11-252 Additional Checks

--- Section_11_File_Image_Upload/controllers/uploadsController.js
+++ Section_11_File_Image_Upload/controllers/uploadsController.js
@@ -1,7 +1,37 @@
+const path = require('path');
 const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
 
 const uploadProductImage = async (req, res) => {
-  res.send('upload product image');
+  if (!req.files) {
+    throw new CustomError.BadRequestError('No file Uploaded');
+  }
+
+  const productImage = req.files.image;
+  const maxSize = 1024 * 1024;
+
+  if (!productImage.mimetype.startsWith('image')) {
+    throw new CustomError.BadRequestError('Please Upload a Image');
+  }
+
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError(
+      'Please Upload Image Smaller than 1KB'
+    );
+  }
+
+  const imagePath = path.join(
+    __dirname,
+    '../public/uploads/' + `${productImage.name}`
+  );
+
+  await productImage.mv(imagePath);
+
+  return res.status(StatusCodes.OK).json({
+    image: {
+      src: `/uploads/${productImage.name}`,
+    },
+  });
 };
 
 module.exports = { uploadProductImage };
