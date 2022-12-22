eb153845ea93095dabe4b628dc20b848ef681791
Andre Ostan <andre.ostan@gmail.com>
2022-12-21 22:17:44 -0300

    End of Section 11

--- Section_11_File_Image_Upload/controllers/uploadsController.js
+++ Section_11_File_Image_Upload/controllers/uploadsController.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
+const cloudinary = require('cloudinary').v2;
+const fs = require('fs');
 
-const uploadProductImage = async (req, res) => {
+const uploadProductImageLocal = async (req, res) => {
   if (!req.files) {
     throw new CustomError.BadRequestError('No file Uploaded');
   }
@@ -32,6 +34,16 @@ const uploadProductImage = async (req, res) => {
       src: `/uploads/${productImage.name}`,
     },
   });
+};
+
+const uploadProductImage = async (req, res) => {
+  const result = await cloudinary.uploader.upload(
+    req.files.image.tempFilePath,
+    { use_filename: true, folder: 'file-uploader' }
+  );
+
+  fs.unlinkSync(req.files.image.tempFilePath);
+  return res.status(StatusCodes.OK).json({ image: { src: result.secure_url } });
 };
 
 module.exports = { uploadProductImage };
