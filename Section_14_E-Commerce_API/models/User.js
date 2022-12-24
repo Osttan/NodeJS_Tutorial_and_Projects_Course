1bf55702fcfbcfbfafdc3d6fb99125d676addd85
Andre Ostan <andre.ostan@gmail.com>
2022-12-24 18:12:43 -0300

    Section 14-285 User Roles

--- Section_14_E-Commerce_API/models/User.js
+++ Section_14_E-Commerce_API/models/User.js
@@ -10,6 +10,7 @@ const UserSchema = new mongoose.Schema({
   },
   email: {
     type: String,
+    unique: true,
     required: [true, 'Please provide e-mail'],
     validate: {
       validator: validator.isEmail,
