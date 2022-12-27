c4db00b3d5c82e40d9178a193f56a9e689b4cabc
Andre Ostan <andre.ostan@gmail.com>
2022-12-27 16:19:06 -0300

    Section 14-309 Auth Complete

--- Section_14_E-Commerce_API/controllers/authController.js
+++ Section_14_E-Commerce_API/controllers/authController.js
@@ -1,7 +1,7 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
-const { attachCookiesToResponse } = require('../utils');
+const { attachCookiesToResponse, createTokenUser } = require('../utils');
 
 const register = async (req, res) => {
   const { email, name, password } = req.body;
@@ -16,9 +16,9 @@ const register = async (req, res) => {
   const role = isFirstAccount ? 'admin' : 'user';
 
   const user = await User.create({ name, email, password, role });
-  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  const tokenUser = createTokenUser(user);
   attachCookiesToResponse({ res, user: tokenUser });
-  res.status(StatusCodes.CREATED).json({ user: tokenUser });
+  res.status(StatusCodes.OK).json({ user: tokenUser });
 };
 
 const login = async (req, res) => {
@@ -35,7 +35,7 @@ const login = async (req, res) => {
   if (!isPasswordCorrect) {
     throw new CustomError.UnauthenticatedError('Invalid Credentials');
   }
-  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  const tokenUser = createTokenUser(user);
   attachCookiesToResponse({ res, user: tokenUser });
   res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
@@ -45,7 +45,7 @@ const logout = async (req, res) => {
     httpOnly: true,
     expires: new Date(Date.now() + 5000),
   });
-  res.status(StatusCodes.OK).json({ meg: 'user logged out' });
+  res.status(StatusCodes.OK).json({ msg: 'user logged out' });
 };
 
 module.exports = { register, login, logout };
